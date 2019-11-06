d321e8263f1f26d631bbc01a87cabac748f02410
Waqas Galla <waqaskiffal15@gmail.com>
2019-11-06 16:36:21 +0500

    fix: add KeyboardAwareScrollView to show input when keyboard opens

--- src/modules/Auth/SignIn/sign-in-component.tsx
+++ src/modules/Auth/SignIn/sign-in-component.tsx
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import useForm from 'react-hook-form';
 import styled from 'styled-components/native';
 import { Alert } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import TextField from '#components/TextField/text-field-component';
 
 type FormValues = {
@@ -14,6 +15,12 @@ type FormValues = {
 type Props = {
     initialValues: FormValues;
 };
+
+const StyledKeyboardAwareView = styled(KeyboardAwareScrollView).attrs(() => ({
+    contentContainerStyle: {
+        flex: 1
+    }
+}))``;
 
 const Container = styled.View`
     flex: 1;
@@ -42,30 +49,28 @@ export default function SignIn({ initialValues }: Props) {
     });
     const onSubmit = data => Alert.alert('Form Data', data);
     return (
-        <Container>
-            <Headline>Sign In</Headline>
-            <Wrapper>
-                <TextField
-                    label="Email"
-                    ref={register({ name: 'email' })}
-                    onChangeText={text => setValue('email', text)}
-                    errorMsg={errors.email && errors.email.message}
-                />
-                <TextField
-                    label="Password"
-                    secureTextEntry
-                    ref={register({ name: 'password' })}
-                    onChangeText={text => setValue('password', text)}
-                    errorMsg={errors.password && errors.password.message}
-                />
-            </Wrapper>
-            <Button
-                icon="camera"
-                mode="contained"
-                onPress={handleSubmit(onSubmit)}
-            >
-                Sign In
-            </Button>
-        </Container>
+        <StyledKeyboardAwareView extraHeight={200}>
+            <Container>
+                <Headline>Sign In</Headline>
+                <Wrapper>
+                    <TextField
+                        label="Email"
+                        ref={register({ name: 'email' })}
+                        onChangeText={text => setValue('email', text)}
+                        errorMsg={errors.email && errors.email.message}
+                    />
+                    <TextField
+                        label="Password"
+                        secureTextEntry
+                        ref={register({ name: 'password' })}
+                        onChangeText={text => setValue('password', text)}
+                        errorMsg={errors.password && errors.password.message}
+                    />
+                </Wrapper>
+                <Button mode="contained" onPress={handleSubmit(onSubmit)}>
+                    Sign In
+                </Button>
+            </Container>
+        </StyledKeyboardAwareView>
     );
 }
